7e28e9983eae143c62ed18a0da2648d7a0fa829c
nhocbangchu95 <duchv2307@gmail.com>
2016-11-14 17:06:25 +0700

    Manager review

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, alert: exception.message
+    redirect_to root_url
+    flash[:danger] = t "controller.permission"
   end
 
   rescue_from ActiveRecord::RecordNotFound do
--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,15 +1,7 @@
 class ToursController < ApplicationController
-  before_action :find_tour, only: :show
+  load_and_authorize_resource
 
   def show
-  end
-
-  private
-  def find_tour
-    @tour = Tour.find_by_id params[:id]
-    unless @tour
-      flash[:danger] = t "tours.not_found"
-      redirect_to root_path
-    end
+    @reviews = @tour.reviews
   end
 end

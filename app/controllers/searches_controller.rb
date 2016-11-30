f43a7b6df84993f736bffad03a65b51132e111c1
nguyenquocthong <quocthong9592@gmail.com>
2016-11-30 23:48:53 +0700

    fix-app

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,8 +1,9 @@
 class SearchesController < ApplicationController
   def index
     q = params[:search]
-    @places = Place.search(name_cont: q).result
-    @tours = Tour.search(name_cont: q).result
-      .includes(:place)
+    @tours = Tour.search(name_cont: q).result.
+      page(params[:page]).per Settings.tour
+    @search= Tour.search(params[:q])
+    @search.build_sort
   end
 end
